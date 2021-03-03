d179da25c5643516cf1f6570ef564e749226b8d9
Cristian-byte <andygign40@gmail.com>
2021-03-02 23:02:49 -0600

    Creando la función de editar un post

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -60,6 +60,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
+        return view('posts.edit', compact('post'));
     }
     /**
      * Show the form for editing the specified resource.
